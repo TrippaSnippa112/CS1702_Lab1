1bbecca51616f34377009b87dc02f299e5fb4798
TrippaSnippa112 <40662792+TrippaSnippa112@users.noreply.github.com>
2019-03-26 07:18:04 +0000

    Add files via upload

---
+++ CS1702_Lab1.java
@@ -0,0 +1,30 @@
+
+public class CS1702_Lab1 
+{
+	public static void main(String args[]) 
+	{
+		// System.out.println("Hello World!"); //Simply printlns text into console window
+		
+		Method1(); //Will search for Method1 and follow tasks set for it
+		Method2();
+		Method4();
+		Method3();
+		Method2();
+	}
+	public static void Method1()
+	{
+		System.out.println("Hello from Method1!!!");
+	}
+	public static void Method2()
+	{
+		System.out.println("Hello from Method2!!!");
+	}
+	public static void Method3()
+	{
+		System.out.println("Hello from Method3!!!");
+	}
+	public static void Method4()
+	{
+		System.out.println("Hello from Method4!!!");
+	}
+}
